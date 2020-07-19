fe8f750deb941c731af56cc5fe857706d33e8fda
tatchan.rb <43107628+TATCHAN0825@users.noreply.github.com>
2020-07-19 21:08:30 +0900

    もっといい実装あるんだろうけどこれで

--- lobinotspwanentiy/src/tatchan/lobinotspwanentiy/Main.php
+++ lobinotspwanentiy/src/tatchan/lobinotspwanentiy/Main.php
@@ -5,25 +5,38 @@ declare(strict_types=1);
 namespace tatchan\lobinotspwanentiy;
 
 use pocketmine\event\entity\EntitySpawnEvent;
+use pocketmine\event\Listener;
+use pocketmine\math\Vector3;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase
+class Main extends PluginBase implements Listener
 {
 
 	public function onEnable(): void
 	{
 		$this->getLogger()->info("ロビーいるエンティティを消す");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
 	public function onspawn(EntitySpawnEvent $event)
 	{
 		$name = $event->getEntity()->getLevel()->getName();
-		if ($name === "lobby") {
-			$event->setCancelled(true);
+		$entity = $event->getEntity();
+		$x = $event->getEntity()->getx();
+		$y = $event->getEntity()->getY();
+		$z = $event->getEntity()->getZ();
+		$vector = new Vector3($x, $y - 10000, $z);
+		if (!$entity instanceof Player) {
+			if ($name === "lobby") {
+				$entity->teleport($vector);
+				$entity->kill();
+			}
 		}
 	}
 
-	public function onDisable(): void
+	public
+	function onDisable(): void
 	{
 		$this->getLogger()->info("Bye");
 	}
